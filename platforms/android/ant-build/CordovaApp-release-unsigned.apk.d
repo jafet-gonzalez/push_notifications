608bc066dcaf46944a15e004115f815342a58357
jafet gonzalez <detoxikaided@gmail.com>
2015-01-22 01:26:02 -0600

    update android app

---
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,6 @@
+c:\projects\phonegapp\alexion\platforms\android\ant-build\CordovaApp-release-unsigned.apk : \
+c:\projects\phonegapp\alexion\platforms\android\ant-build\CordovaApp.ap_ \
+c:\projects\phonegapp\alexion\platforms\android\ant-build\classes.dex \
+C:\projects\phonegapp\alexion\platforms\android\CordovaLib\ant-build\classes.jar \
+c:\projects\phonegapp\alexion\platforms\android\libs\gcm.jar \
+c:\projects\phonegapp\alexion\platforms\android\libs\android-support-v13.jar \

ce03a738626ab47ae8a4622e6c8d173eeb490c8d
jafet gonzalez <detoxikaided@gmail.com>
2015-01-22 00:48:12 -0600

    second commit

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\projects\phonegapp\alexion\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\projects\phonegapp\alexion\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+c:\projects\phonegapp\alexion\platforms\android\ant-build\AndroidManifest.xml : \
+c:\projects\phonegapp\alexion\platforms\android\AndroidManifest.xml \
